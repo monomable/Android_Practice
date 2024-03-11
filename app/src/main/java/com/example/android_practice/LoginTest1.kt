f136edff11d6223c387810ebd388b034a7cf4445
marble-ly <385516@naver.com>
2024-03-11 21:32:20 +0900

    Login Align Test

--- app/src/main/java/com/example/android_practice/LoginTest1.kt
+++ app/src/main/java/com/example/android_practice/LoginTest1.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,49 +16,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class BoxTest1 : ComponentActivity() {
+class LoginTest1 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DividScreen()
 
-
-            /*
-            BoxTest1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }*/
+            // 컨텐츠 출력 부분
         }
     }
 }
 
-
 @Composable
-fun BoxExample0() {  // 좌상단 블루박스
-    Box(
-        modifier = Modifier
-            .background(color = Color.Blue)
-            .size(150.dp, 200.dp)
-    )
-}
-
-@Composable
-fun FullScrean(){  // 전체화면 색칠
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.Red)){
-
-    }
-}
-
-@Composable
-fun DividScreen(){
+fun DividScreen2(){
     Surface()
     {
         Column(modifier = Modifier
@@ -68,36 +38,27 @@ fun DividScreen(){
             horizontalAlignment = Alignment.CenterHorizontally)  // 중앙정렬
         {
             Box(modifier = Modifier
-                .width(100.dp)
-                .background(color = Color.Gray))
-            {
-                Text(text = "Gray Box")
-            }
+                .weight(0.3f)//높이
+            )
 
-            Box(modifier = Modifier
-                .width(80.dp)
-                .weight(0.3f) //높이
-                .background(color = Color.Cyan))
-            {
-                Text(text = "Cyan Box")
-            }
-
-            Text(text = "Yellow Box",
+            Text(text = "알찬 쇼핑",
+                fontSize = 60.sp,
                 modifier = Modifier
-                    .weight(0.5f) //높이
+                    //.weight(0.5f) //높이
                     .background(color = Color.Yellow)
             )
 
             Text(text = "Green Box",
                 modifier = Modifier
                     .weight(1f) //높이
-                    .background(color = Color.Green))
+                    .background(color = Color.Green)
+            )
 
             Box(modifier = Modifier
                 .width(80.dp)
                 .weight(0.5f) //높이
-                .background(color = Color.Cyan))
-            {
+                .background(color = Color.Cyan)
+            ) {
                 Text(text = "Cyan Box")
             }
 
@@ -105,11 +66,9 @@ fun DividScreen(){
     }
 }
 
-/*
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FullScrean()
-    BoxExample0()
-    DividScreen()
-}*/
+    // 프리뷰 부분
+    DividScreen2()
+}
